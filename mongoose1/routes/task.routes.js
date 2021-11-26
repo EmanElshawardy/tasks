f023959342c9a47436e2ebb0de631981d39e089e
emymohamed902 <emmohamed902@gmail.com>
2021-11-26 22:48:23 +0200

    first commit

--- mongoose1/routes/task.routes.js
+++ mongoose1/routes/task.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router()
 const Task = require('../app/controller/task.controller')
 router.post('/add', Task.addTask)
+router.get('/all' ,Task.getAllTasks );
+router.get('/all/:id' ,Task.getSingle );
+router.post('/delete/:id' , Task.deleteTask)
+router.post('/edite/:id' , Task.editTask)
 
 module.exports = router
